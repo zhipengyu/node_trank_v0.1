bff3ef5e21f53a931f25d0565404da027dbf8fcc
marvin <929394361@qq.com>
2019-09-05 10:49:37 +0800

    sdfs

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = `var a=123123;console.log(a)`
+  //   ctx.body=''
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
