8255621dd9b8adf905cb993c6ac1bb9e02545fb7
devin <devin@mobikok.com>
2019-09-06 15:09:38 +0800

    dest

--- config/default.js
+++ config/default.js
@@ -1,9 +1,9 @@
 const config = {
     // 数据库配置
     database: {
-        DATABASE: '',
+        DATABASE: 'dps-collect-koa',
         USERNAME: 'root',
-        PASSWORD: '',
+        PASSWORD: 'mobikok@2018..',
         PORT: '3306',
         HOST: '104.250.131.170'
     }
