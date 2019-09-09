2483723b8225389906246b440c30f27e42778f8d
marvin <929394361@qq.com>
2019-09-09 16:24:18 +0800

    dome

--- controllers/api.js
+++ controllers/api.js
@@ -6,17 +6,15 @@ const fs=require('fs')
 const date=YAML.parse(fs.readFileSync('./mysql.yaml').toString());
 var c={};
 function best() {
-    // console.log(date)
     var reg=[/^GET/gi,/^DELETE/gi,/^POST/gi,/^PUT/gi]
     for(let key in date) {
         c[key] = async (ctx, next) => {
             let _sql = date[key];
             var data = ctx.request.body;
-            if(ctx.params&&data!='undefined'){
+            if(!data){
                 data=ctx.params;
             }
             const _date = stringEs6(_sql, data)
-            console.log(_date)
             const bet = await mysql(_date);
             ctx.rest({
                 data: bet
@@ -35,7 +33,6 @@ function stringEs6(string,data){
     let newString='';
     newString=string.replace(reg,function ($,$1) {
         try {
-            // console.log($1)
             return `\'${eval($1)}\'`;
         }catch (e) {
         }
@@ -44,14 +41,5 @@ function stringEs6(string,data){
     return newString;
 }
 best();
-// const nsat={
-//     'PUT /api/fala':async (ctx,next)=>{
-//         // console.log(ctx.request.body);
-//         let _sql=date
-//         const _date = stringEs6(_sql, data);
-//     }
-// }
-// console.log(best())
-
 module.exports = c;
 
