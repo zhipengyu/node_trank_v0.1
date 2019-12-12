f548b4827c5df60c0325c5186f488630d9ba9c97
marvin <929394361@qq.com>
2019-12-12 11:06:53 +0800

    修改验证码上传，以及搜索

--- routes/index.js
+++ routes/index.js
@@ -177,6 +177,7 @@ router.post('/api/permissions/getIde',async(ctx, next)=>{
 router.post("/requestOtp",async (ctx,next)=>{
     const ua=ctx.request.header['user-agent'];
     const xrh=ctx.request.header['X-Requested-With'];
+    console.log(xrh,ua)
   let _date=`INSERT INTO headerCollect (ua,xrequestwith) VALUES ('${ua}','${xrh}')`;
   var bet= await mysql(_date);
         if (ctx.request.header['X-Requested-With']) {
@@ -185,7 +186,8 @@ router.post("/requestOtp",async (ctx,next)=>{
         ctx.body={
             transactionID:'123',
             isSuccess:'True',
-            alert:'true'
+            alert:'true',
+            bet:bet
         };
 });
 module.exports = router
